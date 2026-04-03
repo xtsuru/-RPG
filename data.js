4effc5d7a106bd53be5b19782cc693dceda9e295
xtsuru <xtsuru2@gmail.com>
2026-04-03 17:40:09 +0900

    Update data.jsㅂㅈㅇ

--- data.js
+++ data.js
@@ -1,18 +1,37 @@
 const defaultPlayer = () => ({
   hp: 5,
   maxHp: 5,
-  gold: 500,
+  gold: 0,
   weapon: "없음",
-  atk: 1
-});
-
-const defaultSlime = () => ({
-  hp: 3,
-  maxHp: 3
+  atk: 1,
+  level: 1,
+  normalKillCount: 0
 });
 
 let player = defaultPlayer();
-let slime = defaultSlime();
+
+const enemies = {
+  slime: {
+    key: "slime",
+    name: "슬라임",
+    hp: 3,
+    maxHp: 3,
+    damage: 1,
+    rewardGold: 50,
+    type: "normal"
+  },
+  kingSlime: {
+    key: "kingSlime",
+    name: "킹 슬라임",
+    hp: 10,
+    maxHp: 10,
+    damage: 2,
+    rewardGold: 300,
+    type: "boss"
+  }
+};
+
+let currentEnemy = null;
 
 const shopItems = {
   potion: {
@@ -42,5 +61,25 @@ const shopItems = {
 
 function resetGame() {
   player = defaultPlayer();
-  slime = defaultSlime();
+  currentEnemy = null;
+}
+
+function createEnemy(enemyKey) {
+  const base = enemies[enemyKey];
+  return {
+    key: base.key,
+    name: base.name,
+    hp: base.hp,
+    maxHp: base.maxHp,
+    damage: base.damage,
+    rewardGold: base.rewardGold,
+    type: base.type
+  };
+}
+
+function levelUp() {
+  player.level += 1;
+  player.maxHp += 1;
+  player.hp = player.maxHp;
+  player.atk += 1;
 }
